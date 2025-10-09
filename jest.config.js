9c2d2c5055267aca1799b88e0b46ffdd576adfe9
Conner Krenzer <krenzer84@gmail.com>
2025-10-09 11:16:11 -0400

    tests created

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,13 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
-  testMatch: [
-    '<rootDir>/tests/**/*.test.js'
-  ],
-  collectCoverageFrom: [
-    'scripts/**/*.js',
-    'server.js',
-    '!**/node_modules/**'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+  testMatch: ["<rootDir>/tests/**/*.test.js"],
+  collectCoverageFrom: ["scripts/**/*.js", "server.js", "!**/node_modules/**"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
   testTimeout: 10000,
   globals: {
-    TextEncoder: require('util').TextEncoder,
-    TextDecoder: require('util').TextDecoder,
-  }
+    TextEncoder: require("util").TextEncoder,
+    TextDecoder: require("util").TextDecoder,
+  },
 };
